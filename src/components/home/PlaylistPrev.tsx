04a1314b843c66d25362abf5793a7b88971c1c8b
LeeJY97 <ijoonyeol97@gmail.com>
2024-10-15 17:39:15 +0900

    fix: 플레이리스트 목록 호스트 id 기준으로 가져오도록 변경

--- src/components/home/PlaylistPrev.tsx
+++ src/components/home/PlaylistPrev.tsx
@@ -1,3 +1,4 @@
+import { useGetUserIds } from "@/app/[userId]/setting/hooks/useGetUserIds";
 import { getPlaylist } from "@/app/[userId]/setting/server-action/playlistAction";
 import queryKey from "@/queries/queryKey";
 import {  useUserInfoStore } from "@/store/useUserInfoStore";
@@ -10,12 +11,13 @@ const hostUserId = "588a4dea-b95a-4836-b6bc-10dbafa4a81f";
 
 const PlaylistPrev = () => {
   // 접속한 유저의 id로 요청하면 안되고, useParams의 id로 요청해얗마
-  const { id: userId } = useUserInfoStore();
+  // const { id: userId } = useUserInfoStore();
+  const {hostUserId, loginUserId} = useGetUserIds();
 
   const { data: playlist } = useQuery<PlayList[]>({
     queryKey: queryKey.playlist(hostUserId),
     queryFn: () => getPlaylist(hostUserId),
-    enabled: !!userId
+    enabled: !!hostUserId
   });
 
   if (!playlist) {
