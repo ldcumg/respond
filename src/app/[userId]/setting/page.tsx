74fa83caf710c65833abc682e1b325a8151f1e72
LeeJY97 <ijoonyeol97@gmail.com>
2024-10-15 11:46:42 +0900

    feat: 동적 라우팅 설정

---
+++ src/app/[userId]/setting/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import React from "react";
+import SettingPrivacy from "./components/SettingPrivacy";
+import { getSetting } from "./server-action/settingAction";
+import { useQuery } from "@tanstack/react-query";
+import { Setting } from "@/types/setting";
+import queryKey from "@/queries/queryKey";
+import SettingShowList from "./components/SettingShowList";
+import SettingTabList from "./components/SettingTabList";
+import { getLoginUserId } from "@/utils/supabase/user";
+import { useParams } from 'next/navigation'
+import { useRouter } from "next/navigation";
+
+const testUserId = "588a4dea-b95a-4836-b6bc-10dbafa4a81f";
+
+const page = () => {
+  const {userId:hostUserId} = useParams<{ userId:string; }>();
+  const router = useRouter()
+
+  const { data: loginUserId } = useQuery<string | undefined>({
+    queryKey: queryKey.auth.loginUser,
+    queryFn: () => getLoginUserId()
+  });
+
+
+  const { data: setting } = useQuery<Setting>({
+    queryKey: queryKey.setting.setting,
+    queryFn: () => getSetting(testUserId),
+    enabled: !!loginUserId
+  });
+
+
+  if (!setting) {
+    return <></>;
+  }
+
+  if (loginUserId !== hostUserId) {
+    const href = `/${hostUserId}/`
+    router.replace(href);
+  }
+
+  return (
+    <div>
+      <SettingPrivacy setting={setting}></SettingPrivacy>
+      <SettingShowList setting={setting}></SettingShowList>
+      <SettingTabList setting={setting}></SettingTabList>
+    </div>
+  );
+};
+
+export default page;
--- src/app/setting/page.tsx
+++ src/app/setting/page.tsx
@@ -1,39 +1,9 @@
-"use client";
-
-import React from "react";
-import SettingPrivacy from "./components/SettingPrivacy";
-import { getSetting } from "./server-action/settingAction";
-import { useQuery } from "@tanstack/react-query";
-import { Setting } from "@/types/setting";
-import queryKey from "@/queries/queryKey";
-import SettingShowList from "./components/SettingShowList";
-import SettingTabList from "./components/SettingTabList";
-import { useUserInfoStore } from "@/store/useUserInfoStore";
-
-const testUserId = "588a4dea-b95a-4836-b6bc-10dbafa4a81f";
+import React from 'react'
 
 const page = () => {
-  const { id: userId } = useUserInfoStore();
-
-  const { data: setting } = useQuery<Setting>({
-    queryKey: queryKey.setting.setting,
-    queryFn: () => getSetting(testUserId),
-    enabled: !!userId
-  });
-
-  //TODO: 조건 하나 더 추가 해야함 (params랑 로그인 userId 다른 다른 페이지로)
-
-  if (!setting) {
-    return <></>;
-  }
-
   return (
-    <div>
-      <SettingPrivacy setting={setting}></SettingPrivacy>
-      <SettingShowList setting={setting}></SettingShowList>
-      <SettingTabList setting={setting}></SettingTabList>
-    </div>
-  );
-};
+    <div>ASDFMKLASDFKML</div>
+  )
+}
 
-export default page;
+export default page
