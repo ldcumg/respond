536308f3d41d39b09d1fb6b8fbb08af956419118
ldcumg <wonbin145@gmail.com>
2024-10-16 18:23:54 +0900

    update: 작성자 로그인한 유저 정보 적용

--- src/components/post/postList.tsx
+++ src/components/post/postList.tsx
@@ -18,26 +18,27 @@ const PostList = ({ userId }: Props) => {
     return <div>오류가 발생했습니다.</div>;
   }
 
-  const posts = data.pages.flat()
+  const posts = data.pages.flat();
 
   return (
-    <ol className="flex flex-col h-5/6 scroll-auto">
+    <ol className="flex h-4/5 flex-col overflow-auto">
       {posts.map((post) => {
         const createdDay = post.created_at.substring(0, 10);
         return (
           <Link
             href={`/${userId}/board/${post.id}`}
             key={post.id}
-            className="flex h-11 flex-row items-center justify-between border-b-2"
-          >
-            <h6 className="text-xl">{post.title}</h6>
+            className="flex flex-row items-center justify-between border-b-2 py-3 hover:bg-gray-50">
+            <h6 className="h-full text-xl">{post.title}</h6>
             <p>
               <small>{createdDay}</small>
             </p>
           </Link>
         );
       })}
-      <button className="mt-1" onClick={() => fetchNextPage()}>더 보기</button>
+      <button className="mt-4 hover:bg-gray-50" onClick={() => fetchNextPage()}>
+        더 보기
+      </button>
     </ol>
   );
 };
